76c1327a293f5eeadd2acf9de0948a5111493f48
Jonas <jonaselemented@gmail.com>
2024-12-31 07:42:38 -0800

    new commit

--- components/Project.tsx
+++ components/Project.tsx
@@ -51,7 +51,7 @@ function Project({
 
         <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-500 z-20"></div>
 
-        <div className="absolute inset-0 flex justify-center items-center gap-3 opacity-0 translate-y-full group-hover:opacity-100 group-hover:translate-y-0 transition-all duration-500 z-30">
+        <div className="absolute inset-0 flex justify-center items-center gap-3 opacity-0 translate-y-full group-hover:opacity-100 group-hover:translate-y-0 transition-all duration-500 z-30 flex-wrap">
           <Link
             href={githubLink}
             target="_blank"
